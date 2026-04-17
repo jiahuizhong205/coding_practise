73be4fa2a2e415e6e52c0006cb8378b11ce0345f
chengwuyouxin <241880299@smail.nju.edu.cn>
2026-04-17 13:58:42 +0800

    feat: 26_4_17

---
+++ leetcode_26_4/2187_minimumTime.cpp
@@ -0,0 +1,46 @@
+//
+// Created by zhongjiahui on 26-4-17.
+//
+/*
+给你一个数组 time ，其中 time[i] 表示第 i 辆公交车完成 一趟旅途 所需要花费的时间。
+每辆公交车可以 连续 完成多趟旅途，也就是说，一辆公交车当前旅途完成后，可以 立马开始 下一趟旅途。每辆公交车 独立 运行，也就是说可以同时有多辆公交车在运行且互不影响。
+给你一个整数 totalTrips ，表示所有公交车 总共 需要完成的旅途数目。请你返回完成 至少 totalTrips 趟旅途需要花费的 最少 时间。
+示例 1：
+输入：time = [1,2,3], totalTrips = 5
+输出：3
+解释：
+- 时刻 t = 1 ，每辆公交车完成的旅途数分别为 [1,0,0] 。
+  已完成的总旅途数为 1 + 0 + 0 = 1 。
+- 时刻 t = 2 ，每辆公交车完成的旅途数分别为 [2,1,0] 。
+  已完成的总旅途数为 2 + 1 + 0 = 3 。
+- 时刻 t = 3 ，每辆公交车完成的旅途数分别为 [3,1,1] 。
+  已完成的总旅途数为 3 + 1 + 1 = 5 。
+所以总共完成至少 5 趟旅途的最少时间为 3 。
+ */
+#include <algorithm>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    long long sum(vector<int> &nums,long long time) {
+        long long sum=0;
+        for(int i=0;i<nums.size();i++) {
+            sum += time / nums[i];
+        }
+        return sum;
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        int n = time.size();
+        long long left = 0,right = (long long)*min_element(time.begin(), time.end()) * totalTrips;;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (sum(time,mid) >= totalTrips) {
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
+
+    }
+};
