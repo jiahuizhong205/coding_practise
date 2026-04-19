8037e95251f8d58a4d6379ca1b50fb96a5075e0d
chengwuyouxin <241880299@smail.nju.edu.cn>
2026-04-19 16:20:33 +0800

    feat: 26_4_19

---
+++ leetcode_26_4/875_minEatingSpeed.cpp
@@ -0,0 +1,38 @@
+//
+// Created by zhongjiahui on 26-4-19.
+//
+/*
+珂珂喜欢吃香蕉。这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 h 小时后回来。
+珂珂可以决定她吃香蕉的速度 k （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 k 根。如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。
+示例 1：
+输入：piles = [3,6,7,11], h = 8
+输出：4
+ */
+#include <algorithm>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int time(vector<int>&piles,int speed) {
+        int res = 0;
+        for (auto pile:piles) {
+            res += (pile + speed - 1)/speed;
+        }
+        return res;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n  = piles.size();
+        int left = 1,right = *max_element(piles.begin(),piles.end());
+        while(left<right) {
+            int mid = (right - left)/2 + left;
+            if (time(piles,mid) <= h) {
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+};
